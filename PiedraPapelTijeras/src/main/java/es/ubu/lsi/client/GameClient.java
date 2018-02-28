8edb45f7bda9fa662c43678ced1c8e65ff1af2c2
Antonio <ama0114@alu.ubu.es>
2018-02-28 16:48:00 +0100

    Comentarios GameClient

--- PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClient.java
+++ PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClient.java
@@ -2,8 +2,28 @@ package es.ubu.lsi.client;
 
 import es.ubu.lsi.common.GameElement;
 
+/**
+ * Interfaz que define la signatura de los métodos de envío de mensaje, 
+ * desconexion y arranque.
+ * @author Antonio de los Mozos Alonso
+ * @author Miguel Angel Leon Bardavio
+ */
 public interface GameClient {
+	
+	/**
+	 * Inicia un cliente
+	 * @return true si se ha podido iniciar, false si no se ha podido iniciar
+	 */
 	public boolean start();
+	
+	/**
+	 * Manda un elemento al servidor
+	 * @param element elemento del tipo GameElement
+	 */
 	public void sendElement(GameElement element);
+	
+	/**
+	 * Desconecta al cliente del servidor
+	 */
 	public void disconnect();
 }
--- PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClientImpl.java
+++ PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClientImpl.java
@@ -2,29 +2,63 @@ package es.ubu.lsi.client;
 
 import es.ubu.lsi.common.GameElement;
 
-public class GameClientImpl {
+/**
+ * Clase que implementa la interfaz GameClient 
+ * y que genera un cliente de juego.
+ * @author Antonio de los Mozos Alonso
+ * @author Miguel Angel Leon Bardavio
+ *
+ */
+public class GameClientImpl implements GameClient {
+	/**
+	 * @param server nombre del servidor
+	 * @param port puerto de conexion del servidor
+	 * @param username nombre de usuario
+	 */
 	public GameClientImpl(String server, int port, String username){
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see es.ubu.lsi.client.GameClient#start()
+	 */
 	public boolean start(){
 		return false;
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see es.ubu.lsi.client.GameClient#sendElement(es.ubu.lsi.common.GameElement)
+	 */
 	public void sendElement(GameElement element){
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see es.ubu.lsi.client.GameClient#disconnect()
+	 */
 	public void disconnect(){
 		
 	}
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args){
 		
 	}
 	
-	private class GameClientListener{
+	/**
+	 * Clase que implementa la interfaz runnable y nos permite 
+	 * generar un hilo de conexión al servidor
+	 * @author Antonio de los Mozos Alonso
+	 * @author Miguel Angel Leon Bardavio
+	 */
+	private class GameClientListener implements Runnable{
+		/**
+		 * Metodo para ejecutar un hilo de escucha de mensajes al servidor
+		 * y mostrar los mensajes entrantes
+		 */
 		public void run(){
 			
 		}
