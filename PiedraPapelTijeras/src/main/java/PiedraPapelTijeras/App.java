4b46d8e846accd4aff5771d0e2ec9988cb036603
Antonio <ama0114@alu.ubu.es>
2018-02-28 15:51:46 +0100

    Inicio proyecto

---
+++ PiedraPapelTijeras/src/main/java/PiedraPapelTijeras/App.java
@@ -0,0 +1,13 @@
+package PiedraPapelTijeras;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
