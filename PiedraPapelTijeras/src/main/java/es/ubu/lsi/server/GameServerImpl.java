3a53adecd7a209c14c10a72958a6cff48abb66f8
Miguel Ángel León Bardavío <mlb0029@alu.ubu.es>
2018-03-05 17:22:35 +0100

    Dia 05/03 Server

--- PiedraPapelTijeras/src/main/java/es/ubu/lsi/server/GameServerImpl.java
+++ PiedraPapelTijeras/src/main/java/es/ubu/lsi/server/GameServerImpl.java
@@ -1,187 +1,133 @@
+/**
+ * 
+ */
 package es.ubu.lsi.server;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.util.concurrent.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import es.ubu.lsi.common.GameElement;
-import es.ubu.lsi.common.GameElement.ElementType;
-import es.ubu.lsi.common.GameResult;
 
+/**
+ * @author Miguel Angel Leon
+ *
+ */
 public class GameServerImpl implements GameServer {
 
-	/**
-	 * Port where is executed the server.
-	 */
 	private int port;
-
-	/**
-	 * List of clients.
-	 */
-	private HashMap<Integer, ServerThreadForClient[]> clientRecord = new HashMap<Integer, ServerThreadForClient[]>();
-
-	private ExecutorService executor = Executors.newCachedThreadPool();
 	
+	private ServerSocket serverSocket;
+	
+	private Boolean serverStatus;
+	
+	private ExecutorService threadExecutor = Executors.newCachedThreadPool();
+	
+	private HashMap<Integer, ServerThreadForClient> clientList = new HashMap<Integer, ServerThreadForClient>();
 	/**
-	 * Constructor of the class that assign the port passed as argument as port
-	 * where is executed the server.
-	 * 
 	 * @param port
-	 *            Port where is executed the server.
 	 */
 	public GameServerImpl(int port) {
 		this.port = port;
 	}
 
-	/**
-	 * Constructor of the class that assign the port 1500 as port where is
-	 * executed the server.
-	 */
 	public GameServerImpl() {
 		this.port = 1500;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/* (non-Javadoc)
 	 * @see es.ubu.lsi.server.GameServer#startup()
 	 */
 	public void startup() {
-
 		try {
-
-			ServerSocket serverSocket = new ServerSocket(this.port);
-			Integer roomNumber = 0;
-
-			while (true) {
+			this.serverSocket = new ServerSocket(this.port);
+			this.serverStatus = true;
+			int idClient = 1;
+			int idRoom = 1;
+			while(serverStatus){
 				Socket clientSocket = serverSocket.accept();
-				ServerThreadForClient thread = new ServerThreadForClient(roomNumber++, clientSocket);
-				// TODO clientRecord.put(arg0, arg1);
+				ServerThreadForClient clientThread = new ServerThreadForClient(clientSocket, idClient, idRoom);
+				if(idClient % 2 == 0)
+					idRoom++;
+				idClient++;
+				
 			}
 		} catch (IOException e) {
 			System.out.println("Listen :" + e.getMessage());
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/* (non-Javadoc)
 	 * @see es.ubu.lsi.server.GameServer#shutdown()
 	 */
 	public void shutdown() {
-		this.executor.shutdown();
+		// TODO Auto-generated method stub
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * es.ubu.lsi.server.GameServer#broadcastRoom(es.ubu.lsi.common.GameElement)
+	/* (non-Javadoc)
+	 * @see es.ubu.lsi.server.GameServer#broadcastRoom(es.ubu.lsi.common.GameElement)
 	 */
 	public void broadcastRoom(GameElement element) {
-		// TODO
+		// TODO Auto-generated method stub
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/* (non-Javadoc)
 	 * @see es.ubu.lsi.server.GameServer#remove(int)
 	 */
 	public void remove(int id) {
-		this.clientRecord.remove(id);
+		// TODO Auto-generated method stub
+
 	}
 
 	/**
-	 * Starts the main thread of the server.
-	 * 
 	 * @param args
-	 *            Not expected to have arguments.
 	 */
 	public static void main(String[] args) {
-		GameServerImpl gameServerImpl = new GameServerImpl();
-		gameServerImpl.startup();
+		GameServerImpl server = new GameServerImpl();
+		server.startup();
 	}
 
 	/**
-	 * The method expects messages from the client and responds with the
-	 * corresponding operation.
-	 * 
-	 * @author Miguel Angel Leon Bardavio
-	 *
-	 */
-	private class ServerThreadForClient extends Thread {
-
-		/**
-		 * A identifier for the room.
+		 * @author Miguel Angel Leon
+		 *
 		 */
-		private int idRoom;
-
-		private ObjectInputStream in;
-
-		private ObjectOutputStream out;
-
+	public class ServerThreadForClient extends Thread {
+		
 		private Socket clientSocket;
-
-		public ServerThreadForClient(int idRoom, Socket clientSocket) {
-
-			try {
-				this.idRoom = idRoom;
-				this.clientSocket = clientSocket;
-				this.in = new ObjectInputStream(this.clientSocket.getInputStream());
-				this.out = new ObjectOutputStream(this.clientSocket.getOutputStream());
-				//this.start();
-			} catch (IOException e) {
-				System.out.println("Connection:" + e.getMessage());
-			}
+		
+		private int idClient;
+		
+		private int idRoom;
+		
+		/**
+		 * @param idRoom
+		 */
+		private ServerThreadForClient(Socket clientSocket, int idClient, int idRoom) {
+			this.clientSocket = clientSocket;
+			this.idClient = idClient;
+			this.idRoom = idRoom;
 		}
 
 		/**
-		 * Gets ID of the room.
-		 * 
-		 * @return ID of the room.
+		 * @return Room ID.
 		 */
 		public int getIdRoom() {
 			return idRoom;
 		}
 
-		/*
-		 * (non-Javadoc)
-		 * 
+		/* (non-Javadoc)
 		 * @see java.lang.Thread#run()
 		 */
 		@Override
 		public void run() {
-			Boolean exitCondition = false;
-
-			try {
-				while (!exitCondition) {
-					GameElement gameElement = (GameElement) in.readObject();
-
-					switch (gameElement.getElement()) {
-
-					case LOGOUT:
-						remove(gameElement.getId());
-						exitCondition = true;
-						break;
-					case SHUTDOWN:
-						shutdown();
-						exitCondition = true;// TODO quizas no sea necesario
-						break;
-					default:
-						broadcastRoom(gameElement);
-						break;
-					}
-
-				}
-			} /*
-				 * catch ( e) { // TODO Cast e.printStackTrace(); }
-				 */catch (IOException e) {
-				System.out.println("Connection:" + e.getMessage());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			// TODO Auto-generated method stub
 		}
+		
 	}
+	
 }
