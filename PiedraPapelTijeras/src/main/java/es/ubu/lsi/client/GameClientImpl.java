27b0026f57d26773aa47c369513a9d78bfe584b8
Antonio <ama0114@alu.ubu.es>
2018-03-05 17:17:50 +0100

    Cliente run

--- PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClientImpl.java
+++ PiedraPapelTijeras/src/main/java/es/ubu/lsi/client/GameClientImpl.java
@@ -1,5 +1,14 @@
 package es.ubu.lsi.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import es.ubu.lsi.common.GameElement;
 
 /**
@@ -10,20 +19,35 @@ import es.ubu.lsi.common.GameElement;
  *
  */
 public class GameClientImpl implements GameClient {
+	
+	private String server;
+	private int port;
+	private String username;
+	private GameClientListener listener;
+	private GameClientImpl impl;
+	
 	/**
 	 * @param server nombre del servidor
 	 * @param port puerto de conexion del servidor
 	 * @param username nombre de usuario
 	 */
 	public GameClientImpl(String server, int port, String username){
-		
+		this.server = server;
+		this.port = port;
+		this.username = username;
+		listener = new GameClientListener();
 	}
 	
 	/* (non-Javadoc)
 	 * @see es.ubu.lsi.client.GameClient#start()
 	 */
 	public boolean start(){
-		return false;
+		try{
+		listener.run();
+		return true;
+		} catch(Exception e){
+			return false;
+		}
 		
 	}
 	
@@ -38,14 +62,14 @@ public class GameClientImpl implements GameClient {
 	 * @see es.ubu.lsi.client.GameClient#disconnect()
 	 */
 	public void disconnect(){
-		
+
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args){
-		
+
 	}
 	
 	/**
@@ -55,12 +79,25 @@ public class GameClientImpl implements GameClient {
 	 * @author Miguel Angel Leon Bardavio
 	 */
 	private class GameClientListener implements Runnable{
+		
 		/**
 		 * Metodo para ejecutar un hilo de escucha de mensajes al servidor
 		 * y mostrar los mensajes entrantes
 		 */
 		public void run(){
-			
+			 try {
+		            Socket s = new Socket(server, port);		            
+		            ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+					System.out.println(in.readObject());
+						
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		           
 		}
 	}
 	
